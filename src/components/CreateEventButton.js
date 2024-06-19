f64255ddf79461b94a5841ece2d46c1dc0c204c4
Matheus <matheus.souza@teamsoft.com.br>
2024-06-19 14:01:25 -0300

    feat: add Sidebar component

---
+++ src/components/CreateEventButton.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function CreateEventButton() {
+  return <button className='border p-2 rounded-full flex items-center shadow-md hover:shadow-2xl'>
+    <span className='material-icons-outlined'>add</span>
+    <span className='pl-3 pr-7'>Novo</span>
+  </button>
+}
